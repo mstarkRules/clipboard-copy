4f5e0f165910008b976bc1aca9dbca97c835717b
Marcos Amorim <markstm10@gmail.com>
2022-03-24 15:25:39 -0300

    refactor: update copy from url

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,15 +1,18 @@
 import React, { useEffect } from "react";
 import { Container } from "./styles";
 
-async function copyFromUrlToClipboard() {
+function copyFromUrlToClipboard() {
   let text = window.location.pathname.split("/");
   navigator.clipboard.writeText(text[1]);
+
+  console.log("rtexttt: ", text[1]);
+  return text[1];
 }
 // copyFromUrlToClipboard();
 
 export function SuccessPage() {
-  useEffect(() => {
-    copyFromUrlToClipboard();
-  });
-  return <Container>Copiado com sucesso!</Container>;
+  // useEffect(() => {
+  //   copyFromUrlToClipboard();
+  // });
+  return <Container>{copyFromUrlToClipboard()} Copiado com sucesso!</Container>;
 }
