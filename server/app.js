d28e33c3b42df5c69c72c6e68977e99c02c7d768
Marcos Amorim <markstm10@gmail.com>
2022-03-24 14:53:42 -0300

    fix: set procfile and update server

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 let express = require("express");
 const path = require("path");
 
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
 
 let app = express();
 app.use(express.static("dist"));
