c4f85aaac5b5824ddb2aae1ff30f25743605a3d7
Marcos Amorim <markstm10@gmail.com>
2022-03-24 02:51:03 -0300

    fix: set port and server heroku config

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 let express = require("express");
 const path = require("path");
-const port = process.env.PORT;
+
+const port = process.env.PORT | 1337;
 
 let app = express();
 app.use(express.static("dist"));
 
 app.get("*", (req, res) => res.sendFile(path.resolve("dist", "index.html")));
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Servidor iniciado na porta", port);
 });
