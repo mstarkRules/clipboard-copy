938429e3cfbc05ce0fadaab2c82181217dcfa8f8
Marcos Amorim <markstm10@gmail.com>
2022-03-24 03:36:04 -0300

    fix: update server.js

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
 let express = require("express");
 const path = require("path");
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT | 5000;
 
 let app = express();
 app.use(express.static("dist"));
 
 app.get("*", (req, res) => res.sendFile(path.resolve("dist", "index.html")));
 
-app.listen(port, function () {
+app.listen(port, "0.0.0.0", function () {
   console.log("Servidor iniciado na porta", port);
 });
