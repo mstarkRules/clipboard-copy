36b8ca32022773ca56e6d4b88930e0a1ca6f0087
Marcos Paulo Amorim <markstm10@gmail.com>
2025-01-08 12:07:27 -0300

    feat: create button to go to home from success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,14 +1,35 @@
 import React from "react";
 import { Container } from "./styles";
+import { decodeText } from "../../lib/utils";
+import CopyButton from "../../components/CopyButton";
+import Button from "../../components/Button";
+import { MainContainer } from "../Home/styles";
 
 function getText() {
   let urlToText = window.location.pathname.split("/")[1];
+  const decodedText = decodeText(urlToText);
 
-  return urlToText;
+  console.log(`original text: ${urlToText}`);
+
+  console.log(`decoded text: ${decodedText}`);
+
+  return decodedText;
 }
 
 function SuccessPage() {
-  return <Container>{getText()} Copiado com sucesso!</Container>;
+  function handleGoToHome() {
+    window.location.href = "/";
+  }
+  return (
+    <Container>
+      <MainContainer>
+        "{getText()}" Copiado com sucesso!
+        <Button onClick={handleGoToHome} title="Gerar novo texto">
+          Gerar novo texto
+        </Button>
+      </MainContainer>
+    </Container>
+  );
 }
 
 export default SuccessPage;
